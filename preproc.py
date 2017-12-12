e1156b5827651587f452c028c0beb15bfd562129
Emily Peterson <egp@Emilys-MacBook-Pro.local>
2017-12-12 11:30:52 -0500

    copied NIBL lab repository

---
+++ preproc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 10:54:00 2017
+
+@author: egp
+"""
+
+#list modules to import
+import glob
+import os
+import shutil
+
+def prepro(basedir):
+    #do my preporcessing
+    print("Data are in the path " +basedir)
+    
+
+def main(): #to run our global variables and execute our functions
+    basedir = '/Users/egp/Desktop/uncworkshop/data'
+    prepro(basedir)
+
+main()
